2b5dd0f8e24126a285890dbc9d69080e5cda92c7
varsheni-2006 <rajavarsheni@gmail.com>
2025-10-28 23:03:56 +0530

    Create pi.jsx

---
+++ src/pi.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Fetching Data from API</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
